a1e4175dd6ecd244adc9ae4e08dc39fc8b776c22
Jay Fresh <phemmyteelectronics@gmail.com>
2023-08-05 20:18:33 +0100

    modify home page

--- app/page.js
+++ app/page.js
@@ -1,10 +1,7 @@
-import Navbar from "@components/common/Navbar";
-
 export default function Home() {
   return (
     <>
-      <Navbar />
-      <div className="mt-[70px]">Home</div>
+      <div className="">Landing Page</div>
     </>
   );
 }
